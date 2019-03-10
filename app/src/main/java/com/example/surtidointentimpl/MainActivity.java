91dad0a4c3b0aa2347387c279c218c92ddd5555f
mpa19 <marcperezarnaiz@gmail.com>
2019-03-10 21:28:22 +0100

    TOT y B1

--- app/src/main/java/com/example/surtidointentimpl/MainActivity.java
+++ app/src/main/java/com/example/surtidointentimpl/MainActivity.java
@@ -11,15 +11,18 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Contacts.People;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener{
-	
+
+	ImageView foto;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -33,6 +36,12 @@ public class MainActivity extends Activity implements OnClickListener{
 	    Button btn5 = findViewById(R.id.button5);
 	    Button btn6 = findViewById(R.id.button6);
 		Button btn7 = findViewById(R.id.buttonM);
+		Button btn8 = findViewById(R.id.buttonSms);
+		Button btn9 = findViewById(R.id.button7);
+		Button btn10 = findViewById(R.id.button8);
+
+
+
 
 
 		btn1.setOnClickListener(this);
@@ -42,8 +51,14 @@ public class MainActivity extends Activity implements OnClickListener{
 	    btn5.setOnClickListener(this);
 	    btn6.setOnClickListener(this);
 	    btn7.setOnClickListener(this);
-		
-	    if (Build.VERSION.SDK_INT >= 23)
+		btn8.setOnClickListener(this);
+		btn9.setOnClickListener(this);
+		btn10.setOnClickListener(this);
+
+
+		foto = findViewById(R.id.imageView);
+
+		if (Build.VERSION.SDK_INT >= 23)
 	        if (! ckeckPermissions())
 		   requestPermissions();	  
 	}
@@ -57,45 +72,70 @@ public class MainActivity extends Activity implements OnClickListener{
 		final String textoABuscar = "escuela politecnica superior";
 
 		switch (v.getId()) {
-	    case R.id.button1: 
-	        Toast.makeText(this, getString(R.string.opcio1), Toast.LENGTH_LONG).show();
-	        in = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + lat + ',' + lon));
-	        startActivity(in);
-	        break;
-	    case R.id.button2: 
-		Toast.makeText(this, getString(R.string.opcio2), Toast.LENGTH_LONG).show();
-		in = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + adressa));
-		startActivity(in);
-	        break;
-	    case R.id.button3:
-	    	Toast.makeText(this, getString(R.string.opcio3), Toast.LENGTH_LONG).show();
-		in = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-		startActivity(in);
-	        break;
-	    case R.id.button4:
-	    	Toast.makeText(this, getString(R.string.opcio4), Toast.LENGTH_LONG).show();
-		//in = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com/search?q=" + "escola politecnica superior UdL"));
-		in = new Intent(Intent.ACTION_WEB_SEARCH);
-		in.putExtra(SearchManager.QUERY, textoABuscar);
-		startActivity(in);
-	        break;
-	    case R.id.button5:
-	    	Toast.makeText(this, getString(R.string.opcio5), Toast.LENGTH_LONG).show();
-		in = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + getText(R.string.telef)));
-		startActivity(in);
-	        break;
-	    case R.id.button6:
-	    	Toast.makeText(this, getString(R.string.opcio6), Toast.LENGTH_LONG).show();
-		in = new Intent(Intent.ACTION_VIEW);
-                in.setData(ContactsContract.Contacts.CONTENT_URI);
-		startActivity(in);
-			break;
-		case R.id.buttonM:
-			in = new Intent(Intent.ACTION_DIAL);
-			in.setData(Uri.parse("tel:"));
-			startActivity(in);
-			break;
-	    }
+			case R.id.button1:
+				Toast.makeText(this, getString(R.string.opcio1), Toast.LENGTH_LONG).show();
+				in = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:" + lat + ',' + lon));
+				startActivity(in);
+				break;
+			case R.id.button2:
+				Toast.makeText(this, getString(R.string.opcio2), Toast.LENGTH_LONG).show();
+				in = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + adressa));
+				startActivity(in);
+				break;
+			case R.id.button3:
+				Toast.makeText(this, getString(R.string.opcio3), Toast.LENGTH_LONG).show();
+				in = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+				startActivity(in);
+				break;
+			case R.id.button4:
+				Toast.makeText(this, getString(R.string.opcio4), Toast.LENGTH_LONG).show();
+				//in = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com/search?q=" + "escola politecnica superior UdL"));
+				in = new Intent(Intent.ACTION_WEB_SEARCH);
+				in.putExtra(SearchManager.QUERY, textoABuscar);
+				startActivity(in);
+				break;
+			case R.id.button5:
+				Toast.makeText(this, getString(R.string.opcio5), Toast.LENGTH_LONG).show();
+				in = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + getText(R.string.telef)));
+				startActivity(in);
+				break;
+			case R.id.button6:
+				Toast.makeText(this, getString(R.string.opcio6), Toast.LENGTH_LONG).show();
+				in = new Intent(Intent.ACTION_VIEW);
+				in.setData(ContactsContract.Contacts.CONTENT_URI);
+				startActivity(in);
+				break;
+			case R.id.buttonM:
+				in = new Intent(Intent.ACTION_DIAL);
+				in.setData(Uri.parse("tel:"));
+				startActivity(in);
+				break;
+			case R.id.buttonSms:
+				in = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:" + getText((R.string.telef))));
+				in.putExtra("sms_body", textoABuscar);
+				startActivity(in);
+				break;
+			case R.id.button7:
+				in = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+				startActivityForResult(in, 2);
+				break;
+			case R.id.button8:
+				in = new Intent(Intent.ACTION_SEND);
+				in.putExtra(Intent.EXTRA_EMAIL, "marcperezarnaiz@gmail.com");
+				in.putExtra(Intent.EXTRA_SUBJECT, "demo");
+				in.putExtra(Intent.EXTRA_TEXT, "test email");
+				in.setType("message/rfc822");
+				startActivity(in);
+				break;
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		super.onActivityResult(requestCode,resultCode,data);
+		if(resultCode == RESULT_OK && requestCode == 2){
+			foto.setImageURI(data.getData());
+		}
 	}
 
 	private boolean ckeckPermissions() {
